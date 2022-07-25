a20fc70f3105b1e4132e3850d722c01409ee1ecb
Hamza Hammadi <hamzahammadi@protonmail.com>
2022-07-25 14:02:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return (
+    s4() +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    s4() +
+    s4()
+  );
+}
+$(document).ready(function () {
+  $("#uuid_p").text(guid());
+  $("#BTN").click(function () {
+    $("#uuid_p").text(guid());
+  });
+});
